1b31e17febb339ec354fc416c1600c8cf81f9af8
Ryota <ryota15765@icloud.com>
2022-12-11 13:50:20 +0900

    fix ble central

--- accelerometerBLE/ble_central/app.js
+++ accelerometerBLE/ble_central/app.js
@@ -1,73 +1,76 @@
-'use strict';
+"use strict";
 
-const noble = require('@abandonware/noble');
-const serviceuuid = `050332b17e594b01b359352779e72bd9`;
+const noble = require("@abandonware/noble");
+const serviceUuid = `050332b07e594b01b359352779e72bd9`;
 const localName = `speedometer`;
 
-const accessChara = (chara) => {
-    console.log('-----Start GATT Access-----')
-    chara.notify(true, (err) => {
-        if (err) {
-          console.log('listen notif error', err)
-        } else {
-          console.log('listen notif')
-        }
-    });
-    chara.on('data', (data, isNotif) => {
-        const jsonStr = data.toString('utf-8');
-        const jsonData = JSON.parse(jsonStr);
-        console.log(jsonData);
-    });
+const parseAccConcatStr = (accConcatStr) => {
+    const accArray = accConcatStr.split(',').map(parseFloat)
+    console.log(accArray);
 }
 
+const accessCharacter = (character) => {
+  character.notify(true, (err) => {
+    if (err) {
+      console.log("listen notif error", err);
+    } else {
+      console.log("listen notif");
+    }
+  });
+  character.on("data", (data, isNotification) => {
+    const accConcatStr = data.toString("utf-8");
+    parseAccConcatStr(accConcatStr);
+  });
+};
 
 //discovered BLE device
 const discovered = (peripheral) => {
-    console.log(`BLE Device Found: ${peripheral.advertisement.localName}(${peripheral.uuid}) RSSI${peripheral.rssi}`);
+  console.log(
+    `BLE Device Found: ${peripheral.advertisement.localName}(${peripheral.uuid})`
+  );
 
-    if(peripheral.advertisement.localName === localName){
-        noble.stopScanning();
-        console.log('device found');
-        console.log(`service discover...`);
+  if (peripheral.advertisement.localName === localName) {
+    noble.stopScanning();
+    console.log("device found");
 
-        peripheral.connect(error => {
-            if (error) {
-                console.log("connection error:", error)
-            } else {
-                console.log("device connected");
-            }
+    peripheral.connect((error) => {
+      if (error) {
+        console.log("connection error:", error);
+      } else {
+        console.log("device connected");
+      }
 
-            peripheral.discoverServices([],(error, services) => {
-                if (error) {
-                    console.log("discover service error", error)
-                }
-                console.log('discover service');               
-                services.forEach(service => {
-                    if(service.uuid === serviceuuid){
-                        service.discoverCharacteristics([], (error, charas) => {
-                            console.log('discover chara');
-                            charas.forEach(chara => {
-                                if(chara.uuid === serviceuuid){
-                                    console.log("found chara: ", chara.uuid)
-                                    accessChara(chara);
-                                }
-                            });
-                        });
-                    }
-                });
+      // Services can not be discoveres also by iOS app
+      peripheral.discoverServices([], (error, services) => {
+        if (error) {
+          console.log("discover service error", error);
+        }
+        console.log("discover service");
+        services.forEach((service) => {
+          console.log(service.uuid);
+          if (service.uuid === serviceUuid) {
+            service.discoverCharacteristics([], (error, characters) => {
+              console.log("discover chara");
+              characters.forEach((character) => {
+                console.log("found character: ", character.uuid);
+                accessCharacter(character);
+              });
             });
+          }
         });
-    }
-}
+      });
+    });
+  }
+};
 
 //BLE scan start
 const scanStart = () => {
-    noble.startScanning();
-    noble.on('discover', discovered);
-}
+  noble.startScanning();
+  noble.on("discover", discovered);
+};
 
-if(noble.state === 'poweredOn'){
-    scanStart();
-}else{
-    noble.on('stateChange', scanStart);
+if (noble.state === "poweredOn") {
+  scanStart();
+} else {
+  noble.on("stateChange", scanStart);
 }
